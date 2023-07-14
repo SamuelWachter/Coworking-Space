0e39bfd65610a2bc9919aa8f12f0afbd220a805e
totoluto <94611749+totoluto@users.noreply.github.com>
2023-07-14 10:49:00 +0200

    fix: added @JsonIgnore to password var --> data leak

--- src/main/java/ch/totoluto/coworkingspace/Entity/User.java
+++ src/main/java/ch/totoluto/coworkingspace/Entity/User.java
@@ -19,6 +19,7 @@ public class User {
     private String email;
 
     @Column(name = "password", nullable = false)
+    @JsonIgnore
     private String password;
 
     @Column(name = "prename", nullable = false, length = 100)
